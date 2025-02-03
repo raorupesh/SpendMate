ecaa26e9a0834a60982027ec87c791f6bc1d2f4d
Karthikeya Panangipalli <karthikeya.panangipalli.02@gmail.com>
2025-02-03 14:16:57 -0800

    Link Groups Page on Home

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-
-import 'screens/home_page.dart';
-import 'screens/login_page.dart';
-import 'screens/signup_page.dart';
+import 'package:spendmate/screens/login_page.dart';
+import 'package:spendmate/screens/signup_page.dart';
+import '../screens/home_page.dart';
+import '../screens/groups_page.dart';
 
 void main() {
   runApp(const SpendMateApp());
@@ -19,16 +19,57 @@ class SpendMateApp extends StatelessWidget {
         primarySwatch: Colors.teal,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      // Start the app on the login page
       initialRoute: '/login',
-      // Change this to /login to start at LoginPage
       routes: {
-        '/': (context) => const HomePage(),
-        // HomePage now only after login/signup
+        '/': (context) => const BottomNavBar(),
         '/login': (context) => const LoginPage(),
         '/signup': (context) => const SignUpPage(),
       },
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+    const GroupsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Groups',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.teal,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
