2e9cd480888e5318b6f160246fea0c5826e0b96b
Karthikeya Panangipalli <karthikeya.panangipalli.02@gmail.com>
2025-02-06 18:43:58 -0800

    Add Split Methods

--- lib/transactions/add_expense_page.dart
+++ lib/transactions/add_expense_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:intl/intl.dart';
 import 'split_method_page.dart';
 
 class AddExpensePage extends StatefulWidget {
